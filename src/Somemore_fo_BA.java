3266966887aa41edbe3be847e2266fc1c63890df
udr013 <udr013@gmail.com>
2018-10-03 01:46:21 +0200

    Resource bundle via properties and List

---
+++ src/Somemore_fo_BA.java
@@ -0,0 +1,12 @@
+import java.util.ListResourceBundle;
+
+public class Somemore_fo_BA extends ListResourceBundle{
+
+	@Override
+	protected Object[][] getContents(){
+		return new Object[][]{
+				{"Hello","foobar!"},
+				{"How_are_you?", "foobarr?"}
+		};
+	}
+}
---
+++ src/com/udr013/localization/ResourceBundles.java
@@ -0,0 +1,29 @@
+package com.udr013.localization;
+
+import java.util.ListResourceBundle;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+public class ResourceBundles{
+
+	public static void main(String[] args){
+
+
+		//Thou it works
+//		ResourceBundle bundle = ListResourceBundle.getBundle("some", new Locale("fr"));
+
+		ResourceBundle bundle = PropertyResourceBundle.getBundle("some", new Locale("fr"));
+		System.out.println(bundle.getString("How_are_you?"));
+		ResourceBundle bundle2 = PropertyResourceBundle.getBundle("some", new Locale("nl", "NL"));
+		System.out.println(bundle2.getString("How_are_you?"));
+
+		ResourceBundle bundle4 = ResourceBundle.getBundle("Somemore", new Locale("fo", "BA"));
+		System.out.println(bundle4.getString("How_are_you?"));
+
+
+		//java.util.MissingResourceException: Can't find resource for bundle java.util.PropertyResourceBundle
+		ResourceBundle bundle3 = ResourceBundle.getBundle("some", new Locale("foo", "BAR"));
+		System.out.println(bundle3.getString("How_are_you?"));
+	}
+}
